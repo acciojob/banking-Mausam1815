4db0ee4266872163a801588a6c43c10bc3c1161e
Mausam Raj <128462297+Mausam1815@users.noreply.github.com>
2023-11-05 13:56:37 +0530

    Done

--- src/main/java/com/driver/CurrentAccount.java
+++ src/main/java/com/driver/CurrentAccount.java
@@ -5,7 +5,12 @@ public class CurrentAccount extends BankAccount{
 
     public CurrentAccount(String name, double balance, String tradeLicenseId) throws Exception {
         // minimum balance is 5000 by default. If balance is less than 5000, throw "Insufficient Balance" exception
+        super(name, balance, 5000);
+        this.tradeLicenseId = tradeLicenseId;
 
+        if(balance < 5000) {
+            throw new Exception("Insufficient Balance");
+        }
     }
 
     public void validateLicenseId() throws Exception {
@@ -14,6 +19,20 @@ public class CurrentAccount extends BankAccount{
         // If the characters of the license Id can be rearranged to create any valid license Id
         // If it is not possible, throw "Valid License can not be generated" Exception
 
+        if(tradeLicenseId == null || tradeLicenseId.length() < 2) {
+            throw new Exception("Valid License can not be generated");
+        }
+        StringBuilder validLicenseId = new StringBuilder();
+        validLicenseId.append(tradeLicenseId.charAt(0));
+        for(int i=1; i<tradeLicenseId.length(); i++) {
+            if(tradeLicenseId.charAt(i) == tradeLicenseId.charAt(i -1 )) {
+                char nextChar = (tradeLicenseId.charAt(i) == 'Z') ? 'A' : (char) (tradeLicenseId.charAt(i) + 1);
+                validLicenseId.append(nextChar);
+            }else {
+                validLicenseId.append(tradeLicenseId.charAt(i));
+            }
+        }
+        tradeLicenseId = validLicenseId.toString();
     }
 
 }
